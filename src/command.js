035df8d85bedc3614139797342e8374ff92e5895
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-03-03 21:20:10 +0400

    created my first cli using yargs!

---
+++ src/command.js
@@ -0,0 +1,91 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import {
+  findNote,
+  getallNotes,
+  newNote,
+  removeAllNotes,
+  removeNote,
+} from "./notes.js";
+
+yargs(hideBin(process.argv))
+  .command(
+    "new <note>",
+    "creates new note!",
+    (yargs) => {
+      return yargs.positional("note", {
+        describe: "the note to create",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const note = await newNote(argv.note);
+      console.log(note);
+    }
+  )
+
+  .command(
+    "all",
+    "gets all notes",
+    () => {},
+    async () => {
+      const allNotes = await getallNotes();
+      console.log(allNotes);
+    }
+  )
+
+  .command(
+    "find <filter>",
+    "finds matched note",
+    (yargs) => {
+      return yargs.positional("filter", {
+        describe: "the filter to find note",
+        type: "string",
+      });
+    },
+    async (argv) => {
+      const hopes = await findNote(argv.filter);
+      console.log(hopes);
+    }
+  )
+  .command(
+    "remove <id>",
+    "remove a note by id",
+    (yargs) => {
+      return yargs.positional("id", {
+        describe: "remove the note based on  id",
+        type: "number",
+      });
+    },
+    async (argv) => {
+      const id = await removeNote(argv.id);
+      console.log("Hope removed: " + id);
+    }
+  )
+  .command(
+    "clean",
+    "remove all notes",
+    () => {},
+    () => {
+      removeAllNotes();
+      console.log("all notes removed!");
+    }
+  )
+
+  .command(
+    "web [port]",
+    "start a web server",
+    (yargs) => {
+      return yargs.positional("port", {
+        describe: "the port to start a web server",
+        type: "number",
+        default: 3000,
+      });
+    },
+    (argv) => {
+      console.log(argv);
+    }
+  )
+
+  .demandCommand(1)
+  .parse();
---
+++ src/notes.js
@@ -0,0 +1,37 @@
+import { getDB, saveDB, insertDB } from "./db.js";
+
+export const newNote = async (note) => {
+  const data = {
+    content: note,
+    id: Date.now(),
+  };
+  await insertDB(data);
+  return data;
+};
+
+export const getallNotes = async () => {
+  const db = await getDB();
+  return db.notes;
+};
+
+export const findNote = async (filter) => {
+  const allNotes = await getallNotes();
+  return allNotes.filter((note) =>
+    note.content.toLowerCase().includes(filter.toLowerCase())
+  );
+};
+
+export const removeNote = async (id) => {
+  const notes = await getallNotes();
+  const matched = notes.find((note) => note.id === id);
+  if (matched) {
+    const newNotes = notes.filter((note) => note.id !== id);
+    await saveDB({ notes: newNotes });
+    return id;
+  }
+  return "Invalid ID, try again!";
+};
+
+export const removeAllNotes = () => {
+  saveDB({ notes: [] });
+};
